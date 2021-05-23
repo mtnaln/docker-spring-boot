cf5e30536582196547382df74ecc17f65f7e26aa
Metin Alnıaçık <metinalniacik@gmail.com>
2021-05-22 20:54:00 -0700

    first commit

---
+++ src/test/java/com/metin/medium/dockerspringboot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metin.medium.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
